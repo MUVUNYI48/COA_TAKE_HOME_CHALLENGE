ea74d82e7a737b910dd0302972bf5a880fd4c0f4
unknown <patrickmuvunyi@gmail.com>
2024-06-07 11:40:24 +0200

    feature: some change on arrayMaps

--- Challenges/arrayMaps.js
+++ Challenges/arrayMaps.js
@@ -1,34 +1,32 @@
-
-//we want to check if the element of the array can summed up to 14
-const target = 14;
-
 function subarraySum(arr, target) {
-    // loop through array
-    for (let i = 0; i < arr.length; i++) {
-      let currentSum = 0;
+    // Initialize the current sum to 0 and the left pointer to 0
+    let currentSum = 0;
+    let left = 0;
   
-      // Loop through the rest of the array starting from the current index
-      for (let j = i; j < arr.length; j++) {
-        // Add the current element to the current sum
-        currentSum += arr[j];
+    // Iterate through the array using the right pointer
+    for (let right = 0; right < arr.length; right++) {
+      // Add the current element to the current sum
+      currentSum += arr[right];
   
-        //return true,If the current sum is equal to the target 
-        if (currentSum === target) {
-          return true;
-        }
+      // While the current sum is greater than the target,
+      // remove elements from the left side of the subarray
+      while (currentSum > target) {
+        currentSum -= arr[left];
+        left++;
+      }
   
-        // If the current sum is greater than the target, break from the inner loop
-        if (currentSum > target) {
-          break;
-        }
+      // If the current sum from subarray is equal to the target then return true
+      if (currentSum === target) {
+        return true;
       }
     }
   
-    // If no subarray is found that sums up to the target, return false
+    // If no subarray is found that sums up to the target,
+    // return false
     return false;
   }
   
   // Example usage
-  const arr = [4, 2, 7];
-
+  const arr = [4, 2, 7, 1, 9, 5];
+  const target = 14;
   console.log(subarraySum(arr, target)); // Output: true
